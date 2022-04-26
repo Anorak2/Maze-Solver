7da3284aa17afe95d20c202770f7189823da2ae6
3064683 <adamcoleberry@gmail.com>
2022-04-25 23:34:44 -0500

    basic gui set up

--- src/main/java/com/example/mazesolver/Main.java
+++ src/main/java/com/example/mazesolver/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MazeSolverGUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 675);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/com/example/mazesolver/MazeSolverController.java
+++ src/main/java/com/example/mazesolver/MazeSolverController.java
@@ -1,18 +1,63 @@
 package com.example.mazesolver;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class MazeSolverController {
     @FXML
-    private Label welcomeText;
+    GridPane mazeGridPane;
 
-    public void initialize(){
+    int[][] mainMaze;
+    int gridSize = 8;
+    int tileSize = 500/gridSize;
 
+    public void initialize() {
+        mainMaze = new int[gridSize][gridSize];
+        mazeGridPane.getChildren().clear();
+        loadPreset(1);
+        for(int x = 0; x < 8; x++){
+            for(int y = 0; y < 8; y++){
+                System.out.print(mainMaze[x][y] + " ");
+            }
+            System.out.println();
+        }
+        displayMaze();
     }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void displayMaze(){
+        mazeGridPane.getChildren().clear();
+
+        for(int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                if(mainMaze[row][col] == 1) {
+                    Rectangle newRectangle = new Rectangle(tileSize, tileSize, Color.web("20bf55"));
+                    mazeGridPane.add(newRectangle, col, row);
+                }
+            }
+        }
     }
+    public void loadPreset(int x){
+        if(x == 1){
+            int[] temp =    {2, 0, 1, 0, 0, 0, 0, 1};
+            mainMaze[0] = temp;
+            temp = new int[]{1, 0, 1, 0, 1, 0, 0, 0};
+            mainMaze[1] = temp;
+            temp = new int[]{0, 0, 0, 0, 1, 0, 0, 0};
+            mainMaze[2] = temp;
+            temp = new int[]{0, 1, 1, 1, 1, 0, 1, 0};
+            mainMaze[3] = temp;
+            temp = new int[]{0, 1, 1, 0, 0, 0, 1, 0};
+            mainMaze[4] = temp;
+            temp = new int[]{0, 0, 1, 0, 1, 1, 1, 0};
+            mainMaze[5] = temp;
+            temp = new int[]{0, 1, 1, 0, 1, 2, 1, 0};
+            mainMaze[6] = temp;
+            temp = new int[]{0, 0, 0, 1, 0, 0, 0, 0};
+            mainMaze[7] = temp;
+        }
+    }
+
 }
